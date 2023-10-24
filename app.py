2c543cc02fa3c9dd6275c75cef8b76623766c2f0
ad-kat <90676982+ad-kat@users.noreply.github.com>
2023-10-24 09:59:47 +0000

    applied dynamic data and API route

--- app.py
+++ app.py
@@ -1,11 +1,34 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
+
+RECOMMENDATIONS = [
+    {
+        'id': 1,
+        'desc': "save more on electricity"
+    },
+    {
+        'id': 2,
+        'desc': "show about a stroll in the park?"
+    },
+    {
+        'id': 3,
+        'desc': "schedule not too busy? try walking to work."
+    },
+    {
+        'id': 4,
+        'desc': "Friend reunion parties? CARPOOL!!!"
+    },
+    {
+        'id': 5,
+        'desc': "sunny day? Lights off in the morning"
+    },
+]
 
 
 @app.route("/")
 def hello_world():
-  return "<p>Hello, World!</p>"
+  return render_template('dashboard.html', recs=RECOMMENDATIONS)
 
 
 if __name__ == "__main__":
